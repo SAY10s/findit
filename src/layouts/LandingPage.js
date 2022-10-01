617c25dfecd08901d1b890d4e7ac99ba24a46fe1
Nikodem Majzner <nikodemmajzner@gmail.com>
2022-10-01 10:30:14 +0200

    landing page IN and OUT animation

--- src/layouts/LandingPage.js
+++ src/layouts/LandingPage.js
@@ -1,18 +1,45 @@
 import Classes from "./styles/LandingPage.module.css";
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const LandingPage = () => {
+  const [isUnloading, setIsUnloading] = useState(false);
+
+  const navigate = useNavigate();
+  function wait(time) {
+    return new Promise((resolve) => {
+      setTimeout(resolve, time);
+    });
+  }
+
+  async function goToPage() {
+    setIsUnloading(true);
+    await wait(1200);
+    navigate(`/Signin`);
+  }
+
   return (
     <div>
-      <div className={Classes.logo}>
+      <div className={`${Classes.logo} ${isUnloading ? Classes.logoOut : ""}`}>
         find<span>.it</span>
       </div>
-      <Link to="Signin">
-        <div className={`${Classes.btn} ${Classes.btn1}`}>GRAJ</div>
-      </Link>
-      <Link to="Signin">
-        <div className={`${Classes.btn} ${Classes.btn2}`}>TWÓRZ</div>
-      </Link>
+      <div
+        className={`${Classes.btn} ${Classes.btn1} ${
+          isUnloading ? Classes.btnOut1 : ""
+        }`}
+        onClick={goToPage}
+      >
+        GRAJ
+      </div>
+      <div
+        className={`${Classes.btn} ${Classes.btn2} ${
+          isUnloading ? Classes.btnOut2 : ""
+        }`}
+        onClick={goToPage}
+      >
+        TWÓRZ
+      </div>
     </div>
   );
 };
--- src/layouts/Signin.js
+++ src/layouts/Signin.js
@@ -1,5 +1,5 @@
 const SignIn = () => {
-  return <div>Sign in</div>;
+  return <div></div>;
 };
 
 export default SignIn;
