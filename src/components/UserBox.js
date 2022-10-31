ed94372b2e4ce5ae2e70bd258c7edfcd1dca83df
Nikodem Majzner <nikodemmajzner@gmail.com>
2022-10-31 14:25:04 +0100

    ADD: gear in userbox

--- src/components/UserBox.js
+++ src/components/UserBox.js
@@ -1,4 +1,5 @@
 import Classes from "./styles/userBox.module.css";
+import SvgSettings from "./svg/SvgSettings";
 
 const UserBox = () => {
   return (
@@ -9,9 +10,11 @@ const UserBox = () => {
       <div className={Classes.pfp}>
         <div className={Classes.pic}></div>
       </div>
-      <div className={Classes.nameAndSurname}>Kim Kituragi</div>
+      <div className={Classes.nameAndSurname}>Kim Kitsuragi</div>
       <div className={Classes.logOut}>Wyloguj się</div>
-      <div className={Classes.gear}>O</div>
+      <div className={Classes.gear}>
+        <SvgSettings />
+      </div>
     </div>
   );
 };
