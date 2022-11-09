3363ffd48f612878a7dac4e1dd45e2373189e36d
Nikodem Majzner <nikodemmajzner@gmail.com>
2022-11-09 23:06:39 +0100

    CHANGE: css for createGame

--- src/layouts/CreateGame.js
+++ src/layouts/CreateGame.js
@@ -1,20 +1,27 @@
 import Classes from "./styles/CreateGame.module.css";
+import { useRef } from "react";
 
 const CreateGame = () => {
+  const titleRef = useRef();
+
   return (
     <div className={Classes.container}>
       <div className={Classes.div1}>Tytuł: </div>
       <div className={Classes.div2}>Opis: </div>
-      <div className={Classes.div3}>Romantyzm</div>
+      <div className={Classes.div3}>
+        <input type={"text"} ref={titleRef} />
+      </div>
       <div className={Classes.div4}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum natus
-        ab veritatis, voluptas accusamus tenetur praesentium officiis omnis unde
-        quas recusandae veniam provident porro? Repudiandae saepe similique a
-        assumenda quod.
+        <input type={"text"} ref={titleRef} />
       </div>
       <div className={Classes.div5}>Autor: </div>
-      <div className={Classes.div6}>Kim Kitsuragi</div>
-      <div className={Classes.div7}>KRYCHA</div>
+      <div className={Classes.div6}>
+        {" "}
+        <input type={"text"} ref={titleRef} />
+      </div>
+      <div className={Classes.div7}>
+        <hr />
+      </div>
       <div className={Classes.div8}>Tabelka</div>
     </div>
   );
