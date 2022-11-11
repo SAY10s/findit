962346f8e11c7fe92146df184a4c134bc4645ad9
Nikodem Majzner <nikodemmajzner@gmail.com>
2022-11-11 17:45:27 +0100

    SYNC to PC

--- src/layouts/CreateGame.js
+++ src/layouts/CreateGame.js
@@ -3,6 +3,8 @@ import { useRef } from "react";
 
 const CreateGame = () => {
   const titleRef = useRef();
+  const descriptionRef = useRef();
+  const authorRef = useRef();
 
   return (
     <div className={Classes.container}>
@@ -12,11 +14,11 @@ const CreateGame = () => {
         <input type={"text"} ref={titleRef} />
       </div>
       <div className={Classes.div4}>
-        <input type={"text"} ref={titleRef} />
+        <input type={"text"} ref={descriptionRef} />
       </div>
       <div className={Classes.div5}>Autor: </div>
       <div className={Classes.div6}>
-        <input type={"text"} ref={titleRef} />
+        <input type={"text"} ref={authorRef} />
       </div>
       <div className={Classes.div7}>
         <hr />
