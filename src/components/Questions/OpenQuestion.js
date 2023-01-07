393340a18cedcb4be1743c16d3ac78544ec95c27
SAY10s <nikodemmajzner@gmail.com>
2023-01-07 20:25:06 +0100

    ADD: opened questions

--- src/components/Questions/OpenQuestion.js
+++ src/components/Questions/OpenQuestion.js
@@ -1,7 +1,64 @@
 import Classes from "../styles/question.module.css";
+import { useRef } from "react";
 
-const OpenQuestion = () => {
-  return <div className={Classes.siur}> Pytanie otwarte</div>;
+const OpenQuestion = (props) => {
+  const questionRef = useRef();
+  const answerRef = useRef();
+  const locationRef = useRef();
+
+  return (
+    <div className={Classes.questionWrapper}>
+      <div className={Classes.top}>
+        <input
+          type={"text"}
+          ref={questionRef}
+          className={Classes.questionInput}
+          placeholder="Pytanie"
+          onChange={() => {
+            props.setData(
+              questionRef.current.value,
+              `02|`,
+              locationRef.current.value
+            );
+          }}
+        />
+        <select>
+          <option value="opened">Otwarte</option>
+          <option value="closed">Zamknięte</option>
+          <option value="milionaires">Milionerzy</option>
+        </select>
+      </div>
+      <div>
+        <input
+          type={"text"}
+          ref={answerRef}
+          className={Classes.answerBLANK}
+          placeholder="Odpowiedź"
+          onChange={() => {
+            props.setData(
+              questionRef.current.value,
+              `02|`,
+              locationRef.current.value
+            );
+          }}
+        />
+        <input
+          type={"text"}
+          ref={locationRef}
+          className={Classes.location}
+          placeholder="Lokalizacja"
+          onChange={() => {
+            props.setData(
+              questionRef.current.value,
+              `02|`,
+              locationRef.current.value
+            );
+          }}
+        />
+        <button className={Classes.delete}>!</button>
+      </div>
+    </div>
+  );
 };
 
 export default OpenQuestion;
